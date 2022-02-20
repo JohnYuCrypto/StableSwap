783d1ff4eb3bee4933a1cbf4948fa467e6d2987b
JohnYuCrypto <john.yu@crypto.com>
2022-02-21 01:02:13 +0800

    update eslint and its config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,26 @@ module.exports = {
   extends: [
     "standard",
     "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    // "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
   rules: {
     "node/no-unsupported-features/es-syntax": [
       "error",
       { ignores: ["modules"] },
     ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        paths: ["node_modules", "types"],
+      },
+    },
   },
 };
